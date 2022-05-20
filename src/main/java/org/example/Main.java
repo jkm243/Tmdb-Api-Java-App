5a8ca5e4dffb9116f8488c1ddeea8ffbcf030fd8
Jkm-Pc2 <jacqueskatsuva@gmail.com>
2022-05-20 14:57:49 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.net.URLEncoder;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
